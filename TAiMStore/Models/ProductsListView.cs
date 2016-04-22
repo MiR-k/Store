28b35a77266586acb4d70735122f1d03bad73b93
MiR_k <antomirk@gmail.com>
2016-04-22 23:04:02 +0300

    add something

---
+++ TAiMStore/Models/ProductsListView.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using TAiMStore.Domain;
+
+namespace TAiMStore.Models
+{
+    public class ProductsListView
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+        public string CurrentCategory { get; set; }
+
+    }
+}
